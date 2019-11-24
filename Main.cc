2887b3e457138ba5c75c0e7bc06f3b3ef65d547e
Parth <developmentprogramming154@gmail.com>
2019-11-24 17:27:25 +0200

    Starting the project

--- Main.cc
+++ Main.cc
@@ -1,37 +1,25 @@
-// This is just all the custom created widget test file
+// This the test file
 
 #include "CGUI.hh"
-
-static void Helloworld(GtkWidget *widget, gpointer data)
-{
-  g_print("Hello World\n");
-}
 
 int main(int argc, char *argv[])
 {
   gtk_init(&argc, &argv);
 
-  Window window(TOPLEVEL, "Gtk Window", CEN);
-  window.Icon("./Image/yaktocat.png");
-  Label label("Text");
-  label.SizeRequest(100, 100);
-  label.Align(LAST, CENTER);
-  Label label2("Test2");
-  Entry entry("Enter something");
-
-  Image image("./Image/yaktocat.png", 400, 400);
-  image.Align(CENTER, CENTER);
-
-  Button button("Button");
-  button.Align(BEGIN, CENTER);
-  button.SignalHandler(CLICKED, G_CALLBACK(Helloworld));
+  Window window(TOPLEVEL, "CGuiWindow", CEN);
 
   Box vbox(VER, 10);
+  vbox.Align(CENTER, CENTER);
+
+  Image image("./Image/gui-interface.png", 300, 300);
+  image.Align(CENTER, BEGIN);
+
+  Label label("Welcome to CGui");
+  label.Align(CENTER, LAST);
+
   vbox.Add<Image>(START, image);
-  vbox.Add<Label>(START, label);
-  vbox.Add<Entry>(END, entry);
-  vbox.Add<Label>(START, label2);
-  vbox.Add<Button>(START, button);
+  vbox.Add<Label>(END, label);
+
   window.Add<Box>(vbox);
   window.InternalWidth(10);
   window.DefaultSize(640, 480);

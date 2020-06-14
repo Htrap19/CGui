f1e9f11bdb72e1b51b66f0c6542cdd6acd6cb005
Parth <developmentprogramming154@gmail.com>
2020-06-14 04:46:39 -0700

    Added more functions to Image widget

--- Widgets/Image/Image.cpp
+++ Widgets/Image/Image.cpp
@@ -2,15 +2,99 @@
 
 namespace CGui
 {
+	Image::Image()
+	{
+		widget = gtk_image_new();
+		this->SetContext(widget);
+	}
+
 	Image::Image(const char* filename)
 	{
 		widget = gtk_image_new_from_file(filename);
 		this->SetContext(widget);
 	}
 
-	Image::Image(const char* filename, int width, int height, bool aspectRatio, GError** error)
+	Image::Image(Pixbuf& pixbuf)
 	{
-		widget = gtk_image_new_from_pixbuf(gdk_pixbuf_new_from_file_at_scale(filename, width, height, aspectRatio, error));
+		widget = gtk_image_new_from_pixbuf(pixbuf.GetWidget());
 		this->SetContext(widget);
+	}
+
+	Image::Image(PixbufAnimation& pixbuf_animation)
+	{
+		widget = gtk_image_new_from_animation(pixbuf_animation.GetWidget());
+		this->SetContext(widget);
+	}
+
+	Image::Image(const char* icon_name, IconSize size)
+	{
+		widget = gtk_image_new_from_icon_name(icon_name, (GtkIconSize)size);
+		this->SetContext(widget);
+	}
+
+	void Image::File(const char* filename)
+	{
+		gtk_image_set_from_file(GTK_IMAGE(widget), filename);
+	}
+
+	void Image::ImagePixbuf(Pixbuf& pixbuf)
+	{
+		gtk_image_set_from_pixbuf(GTK_IMAGE(widget), pixbuf.GetWidget());
+		m_ImagePixbuf = &pixbuf;
+	}
+
+	Pixbuf& Image::ImagePixbuf()
+	{
+		return *m_ImagePixbuf;
+	}
+
+	void Image::Animation(PixbufAnimation& pixbuf_animation)
+	{
+		gtk_image_set_from_animation(GTK_IMAGE(widget), pixbuf_animation.GetWidget());
+		m_PixbufAnimation = &pixbuf_animation;
+	}
+
+	PixbufAnimation& Image::Animation()
+	{
+		return *m_PixbufAnimation;
+	}
+
+	void Image::Iconname(const char* icon_name, IconSize size)
+	{
+		gtk_image_set_from_icon_name(GTK_IMAGE(widget), icon_name, (GtkIconSize)size);
+	}
+
+	IconInfo Image::Iconname()
+	{
+		const char* icon_name;
+		GtkIconSize size;
+		gtk_image_get_icon_name(GTK_IMAGE(widget), &icon_name, &size);
+
+		return { icon_name, (IconSize)size };
+	}
+
+	void Image::Resource(const char* resource_path)
+	{
+		gtk_image_set_from_resource(GTK_IMAGE(widget), resource_path);
+	}
+
+	ImageType Image::StorageType()
+	{
+		return (ImageType)gtk_image_get_storage_type(GTK_IMAGE(widget));
+	}
+
+	void Image::Clear()
+	{
+		gtk_image_clear(GTK_IMAGE(widget));
+	}
+
+	void Image::PixelSize(int pixel_size)
+	{
+		gtk_image_set_pixel_size(GTK_IMAGE(widget), pixel_size);
+	}
+
+	int Image::PixelSize()
+	{
+		return gtk_image_get_pixel_size(GTK_IMAGE(widget));
 	}
 };
--- Widgets/Image/Image.hh
+++ Widgets/Image/Image.hh
@@ -7,7 +7,26 @@ namespace CGui
 	class Image : public Widget
 	{
 	public:
+		Image();
 		Image(const char* filename);
-		Image(const char* filename, int width, int height, bool aspectRatio = true, GError** error = NULL);
+		Image(Pixbuf& pixbuf);
+		Image(PixbufAnimation& pixbuf_animation);
+		Image(const char* icon_name, IconSize size);
+		void File(const char* filename);
+		void ImagePixbuf(Pixbuf& pixbuf);
+		Pixbuf& ImagePixbuf();
+		void Animation(PixbufAnimation& pixbuf_animation);
+		PixbufAnimation& Animation();
+		void Iconname(const char* icon_name, IconSize size);
+		IconInfo Iconname();
+		void Resource(const char* resource_path);
+		ImageType StorageType();
+		void Clear();
+		void PixelSize(int pixel_size);
+		int PixelSize();
+
+	protected:
+		Pixbuf* m_ImagePixbuf;
+		PixbufAnimation* m_PixbufAnimation;
 	};
 }
